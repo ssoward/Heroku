2cd418d3807a6a3c85ec590153b5ba9841d0e562
Scott Soward <amorvivir@yahoo.com>
2012-12-17 13:30:16 -0700

    asd

---
+++ target/.generated/com/jsimone/client/Messages_.java
@@ -0,0 +1,12 @@
+package com.jsimone.client;
+
+public class Messages_ implements com.jsimone.client.Messages {
+  
+  public java.lang.String nameField() {
+    return "Enter your name";
+  }
+  
+  public java.lang.String sendButton() {
+    return "Send";
+  }
+}
---
+++ target/generated-sources/gwt/com/jsimone/client/Messages.java
@@ -0,0 +1,26 @@
+package com.jsimone.client;
+
+/**
+ * Interface to represent the messages contained in resource bundle:
+ * 	/Users/ssoward/sandbox/workspace/heroku4/src/main/resources/com/jsimone/client/Messages.properties'.
+ */
+public interface Messages extends com.google.gwt.i18n.client.Messages {
+  
+  /**
+   * Translated "Enter your name".
+   * 
+   * @return translated "Enter your name"
+   */
+  @DefaultMessage("Enter your name")
+  @Key("nameField")
+  String nameField();
+
+  /**
+   * Translated "Send".
+   * 
+   * @return translated "Send"
+   */
+  @DefaultMessage("Send")
+  @Key("sendButton")
+  String sendButton();
+}
